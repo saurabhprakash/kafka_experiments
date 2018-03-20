871ef64232e11c5b1383575b545882da53ef5ca3
saurabh <saurabh@kuliza.com>
2018-03-20 15:47:18 +0530

    read/send json data

--- sample_python_kafka_module/consumer.py
+++ sample_python_kafka_module/consumer.py
@@ -1,21 +1,30 @@
+import json
+
 from flask import Flask, Response
 from kafka import KafkaConsumer
 
-#connect to Kafka server and pass the topic we want to consume
-consumer = KafkaConsumer('test', group_id='view', 
-    bootstrap_servers=['0.0.0.0:9092'], auto_offset_reset='earliest')
+# Current consumer always picks those tasks post past processed offset.
+consumer = KafkaConsumer(group_id='test', bootstrap_servers='localhost:9092',
+    value_deserializer=lambda m: json.loads(m.decode('ascii')))
 
-#Continuously listen to the connection and print messages as recieved
+consumer.subscribe(['test'])
+
+# Continuously listen to the connection and print messages as recieved
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return Response(kafkastream())
 
+
 def kafkastream():
-    for msg in consumer:
-        print (msg.value)
-        yield (msg.value + '\n')
+    for message in consumer:
+        print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+            message.offset, message.key, message.value))
+        # consumer.commit()
+        yield (str(message.value) + '\n')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- sample_python_kafka_module/producer.py
+++ sample_python_kafka_module/producer.py
@@ -1,21 +1,26 @@
 # producer.py
-
 import time
-from kafka import KafkaProducer, KafkaClient
-from PIL import Image
+import json
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+from kafka import KafkaProducer
+
+# producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 # Assign a topic
 topic = 'test'
 
+
 def emitter():
     print(' emitting.....')
     while True:
-        producer.send(topic, str(time.time()))
+        _time = str(time.time())
+        producer.send(topic, {'foo': _time})
+        # producer.send(topic, str(time.time()))
         time.sleep(2)
     print('done emitting')
 
+
 if __name__ == '__main__':
     emitter()
-
